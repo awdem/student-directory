bfb6b687283015750a129283acbd80f25f7265fc
William de Montmollin <wdemontmollin@gmail.com>
2023-04-05 12:26:51 +0000

    Added ability to take a student list from the command line

--- directory.rb
+++ directory.rb
@@ -3,26 +3,26 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end 
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the student list"
+  puts "4. Load the student list"
   puts "9. Exit" # 9 because we'll add more items
 end
 
@@ -76,8 +76,8 @@ def save_students
   file.close
 end  
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -85,5 +85,17 @@ def load_students
   file.close
 end
 
-interactive_menu
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exist?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end  
 
+try_load_students
+interactive_menu
